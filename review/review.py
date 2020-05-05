9bb02d0665d317e102e61c08fcf7125b3b2b7cba
sandy <95036sandy@gmail.com>
2020-05-05 21:44:48 +0800

    add choose

--- review/review.py
+++ review/review.py
@@ -14,3 +14,12 @@ sum_len = 0
 for d in data:
 	sum_len = sum_len + len(d)
 print('平均留言長度', sum_len/len(data))
+
+#篩選出長度小於100的留言
+new = []
+for d in data:#一次讀一筆
+    if len(d) < 100:
+    	new.append(d)
+print("一共有", len(new), "筆留言長度小於100")
+#讀取第一筆資料
+print(new[0])
